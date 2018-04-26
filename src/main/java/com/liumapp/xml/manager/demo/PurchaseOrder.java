e7fbb9a1ede2b081d2fb3f4c40c0498f0bcf439c
liumapp <liumapp.com@gmail.com>
2018-04-26 09:24:59 +0800

    tmp

--- src/main/java/com/liumapp/xml/manager/demo/PurchaseOrder.java
+++ src/main/java/com/liumapp/xml/manager/demo/PurchaseOrder.java
@@ -1,4 +1,24 @@
 package com.liumapp.xml.manager.demo;
 
+/**
+ * @author liumapp
+ * @file PurchaseOrder.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 4/26/18
+ */
+import java.util.Currency;
+
+import javax.xml.bind.annotation.*;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 public class PurchaseOrder {
+
+ private Double amount;
+
+ @XmlJavaTypeAdapter(CurrencyAdapter.class)
+ private Currency currency;
+
 }
